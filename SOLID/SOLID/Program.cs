da0802ec7d2fd4e9a3d019500ad6462ac0ceb4ca
user <user@LAPTOP-KMNMN9RU>
2025-10-28 18:14:43 +0300

    SOL prensipleri tamamlandı.

---
+++ SOLID/SOLID/Program.cs
@@ -0,0 +1,34 @@
+﻿#region SINGLE RESPONSIBILITY PRINCIPLE(Tek Sorunluluk)
+//// Bir sınıf yalnızca bir sorumluluğa sahip olmalıdır. Değişiklik gereksinimi yanlızca bu sorumluluğa göre olmalıdır
+//using SOLID.SingleResponsibilty;
+
+//StudentSevice studentSevice = new StudentSevice();
+//studentSevice.CreatStuddent();
+
+//MailService mailService = new MailService();
+//mailService.SendMail("Mail konusu", "Mail içerigi");
+
+#endregion
+#region OPEN/ CLOSEDED PRINCIPLE (Açık/Kapalı)
+//// Sınıflar genişlemeye açık, degişikliklere kapalı olmalıdır.
+//using SOLID.OpenClose;
+
+//Daire daire = new Daire(5);
+//Console.WriteLine($"Dairenin Alanı: {daire.Alan()}");
+
+//Kare kare = new Kare(5);
+//Console.WriteLine($"Karenin Alanı: {kare.Alan()}");
+
+#endregion
+#region  LISKOV SUBSTİTUTION PRICIPLE
+// Alt sınıflar, üst sınıfların yerine geçmeli ve sistem doğru şekilde çalışmaya 
+
+using SOLID.LiskovSubstitution;
+
+Eagle eagle = new Eagle();
+eagle.Fly();
+
+Penguen penguen = new Penguen();
+penguen.Fly();
+
+#endregion
---
+++ SOLID/SOLID/SingleResponsibilty/StudentSevice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID.SingleResponsibilty
+{
+    public class StudentSevice
+    {
+        public void CreatStuddent()
+        {
+            Console.WriteLine("Student added.");
+        }
+    }
+}
