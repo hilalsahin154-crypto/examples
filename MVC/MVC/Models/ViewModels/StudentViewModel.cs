9cb916bd85b5064091684b8e0a7b9445b398e6bd
user <user@LAPTOP-KMNMN9RU>
2025-12-14 16:02:42 +0300

    Validatiom ve tag helper yapısı tamamlandı

---
+++ MVC/MVC/Models/ViewModels/StudentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC.Models.ViewModels
+{
+    public class StudentViewModel
+    {
+        [Required(ErrorMessage = "Bu alan boş girilemez")]
+        [DisplayName("Ad Soyad")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Bu alan boş girilemez")]
+        [EmailAddress(ErrorMessage ="E-Posta formatı doğru degildir")]
+        [DisplayName("E-Posta")]
+        public string Email { get; set; }
+
+        [Range(18,60,ErrorMessage="Yaş 18 ile 60 arasi olmalı")]
+        [DisplayName("Yas")]
+        public string Age { get; set; }
+
+    }
+}
