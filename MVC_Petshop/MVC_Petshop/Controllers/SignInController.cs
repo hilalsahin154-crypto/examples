0b92646b935692fdd0755edcbabb83a958d8512b
hilalsahin154-crypto <hilalsahin154@gmail.com>
2026-01-30 21:20:34 +0300

    Giris yap çikiş yap butonu eklendi

---
+++ MVC_Petshop/MVC_Petshop/Controllers/SignInController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Petshop.Models;
+
+namespace MVC_Petshop.Controllers
+{
+    public class SignInController : Controller
+    {
+        private static List<SignIn> users = new()
+        {
+            new SignIn { UserName = "hilalshn_", Password = "0723" }
+        };
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            if (Request.Cookies["LoginUser"] != null)
+                return RedirectToAction("Index", "Home");
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(SignIn model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var user = users.FirstOrDefault(x =>
+                x.UserName == model.UserName &&
+                x.Password == model.Password);
+
+            if (user != null)
+            {
+                Response.Cookies.Append(
+                    "LoginUser",
+                    user.UserName,
+                    new CookieOptions
+                    {
+                        Expires = model.RememberMe
+                            ? DateTimeOffset.Now.AddMinutes(60)
+                            : DateTimeOffset.Now.AddMinutes(20),
+                        HttpOnly = true
+                    });
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            ViewBag.Error = "Kullanıcı adı ya da şifre yanlış.";
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Logout()
+        {
+            Response.Cookies.Delete("LoginUser");
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
+
---
+++ MVC_Petshop/MVC_Petshop/Models/SignIn.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Petshop.Models
+{
+    public class SignIn
+    {
+        [Required(ErrorMessage = "Bu alan boş geçilemez")]
+        [DisplayName("Kullanıcı Adı")]
+        public string UserName { get; set; }   
+
+        [Required(ErrorMessage = "Lütfen şifrenizi giriniz")]
+        [DisplayName("Şifre")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+    }
+}
+
