2cf7eb8b5481eeb988ade76cbf0d3898982e6f5e
hilalsahin154-crypto <hilalsahin154@gmail.com>
2026-02-14 14:57:14 +0300

    SQL yuklendi

---
+++ StudentDemo/StudentDemo.Core/Entities/Author.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StudentDemo.Core.Entities;
+
+public partial class Author
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string? Country { get; set; }
+
+    public DateTime? CreatedAt { get; set; }
+
+    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
+}
