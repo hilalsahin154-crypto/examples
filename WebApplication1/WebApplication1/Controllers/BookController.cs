53d6ffc2b5599f57ddb851c1a2b5dd024f6e2516
hilalsahin154-crypto <hilalsahin154@gmail.com>
2026-02-14 11:41:58 +0300

    GET ve POST yazdım

---
+++ WebApplication1/WebApplication1/Controllers/BookController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models.DTOs;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BookController : Controller
+    {
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var book = new
+            {
+                Id = id,
+                Title = "Örnek Kitap"
+            };
+
+            return Ok(book);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] BookRequestDto request)
+        {
+            return Ok(request);
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Models/DTOs/BookRequestDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebApplication1.Models.DTOs
+{
+    public class BookRequestDto
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}
